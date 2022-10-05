39238a6523537f6f78446e48596940ff62084900
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-10-05 16:02:35 +0900

    PracticesモデルにUserを関連付け

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,2 +1,3 @@
 class Practice < ApplicationRecord
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_032126) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_064902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_032126) do
     t.boolean "important", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["date"], name: "index_practices_on_date", unique: true
+    t.index ["user_id"], name: "index_practices_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_032126) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "practices", "users"
 end
