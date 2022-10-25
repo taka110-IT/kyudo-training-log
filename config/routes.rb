01da138ace18a2d5a248a7dfb50a1722986fa736
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-10-26 02:12:30 +0900

    Deviseのエラーに対応した（Turboに対応するよう修正）

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
   root to: 'practices#index'
   resources :practices
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
