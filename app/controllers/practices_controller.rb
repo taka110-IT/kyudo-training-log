5da3f786fec0475cdb43fcf4039afe3730720c18
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-11-02 18:31:01 +0900

    カレンダーの過去の射数が表示されるよう修正

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -6,6 +6,7 @@ class PracticesController < ApplicationController
   def index
     practices = params[:memos] == 'important' ? Practice.where(important: true) : Practice.all
     @practices = practices.order(fixed: :desc, date: :desc).page(params[:page]).per(10)
+    @calendar_displayed_practices = Practice.all
     start_date = params.fetch(:start_date, Date.today).to_date
     @targets = Target.where(year: start_date.year, month: start_date.month).sum(:total)
     @results = Practice.where(date: start_date.in_time_zone.all_month).sum(:shooting_count)
