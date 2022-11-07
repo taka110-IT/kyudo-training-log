8ab104acfd16c76046b38e1a71d459ec287bb894
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-11-07 19:25:46 +0900

    記録の削除リンクの修正

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -58,7 +58,7 @@ class PracticesController < ApplicationController
     @practice.destroy
 
     respond_to do |format|
-      format.html { redirect_to practices_url, notice: "Practice was successfully destroyed." }
+      format.html { redirect_to practices_url, notice: "Practice was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
     end
   end
