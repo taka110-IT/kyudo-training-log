274a65bcdb1871fc4b0a29a47b398ac1185bf3e9
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-11-08 12:29:54 +0900

    目標入力にHotwireとStimulusを適用

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import ModalController from "./modal_controller"
 application.register("modal", ModalController)
+
+import TargetsModalController from "./targets_modal_controller"
+application.register("targets-modal", TargetsModalController)
