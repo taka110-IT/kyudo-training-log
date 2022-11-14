211f9120fce05bbb11ddd633c1ab2c1fd025b474
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-11-15 08:41:02 +0900

    マイナスの射数は入力できないようにした

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -2,6 +2,7 @@ class Practice < ApplicationRecord
   belongs_to :user
 
   validates :date, uniqueness: { scope: :user_id }
+  validates :shooting_count, numericality: { greater_than_or_equal_to: 0 }
 
   def start_time
     date
