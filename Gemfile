ef7698a9e645789afa81ae24903382ee660f0c78
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-11-29 11:21:15 +0900

    i18n_generatorsを導入して初期設定をした

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ gem "bootsnap", require: false
 gem "simple_calendar", "~> 2.4"
 gem 'devise'
 gem 'kaminari'
+gem 'i18n_generators'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module KyudoTrainingLog
   class Application < Rails::Application
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
