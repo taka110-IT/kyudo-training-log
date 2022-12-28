821edf1a565b0c099a8694049979eb1d9fd27177
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-12-29 01:46:22 +0900

    ログイン失敗時にエラー画面になる問題を修正

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,25 +1,11 @@
 # frozen_string_literal: true
 
 class RegistrationsController < Devise::RegistrationsController
-  def create
-    build_resource(sign_up_params)
-
-    resource.save
+  def destroy
+    resource.destroy
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message! :notice, :destroyed
     yield resource if block_given?
-    if resource.persisted?
-      if resource.active_for_authentication?
-        set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      else
-        set_flash_message! :notice, t('devise.registrations.signed_up_but_inactive')
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource, status: :see_other # 登録失敗時のrespond_withにerror出したいので、ここで303 statusを追加
-    end
+    respond_with_navigational(resource) { redirect_to after_sign_out_path_for(resource_name), status: :see_other, notice: find_message(:destroyed) }
   end
 end
