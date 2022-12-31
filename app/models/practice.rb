76421f0346a2a2052c6b4ed51af6aeb106bda69a
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-12-31 23:15:37 +0900

    記録・目標の射数に上限を設定した

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -2,7 +2,7 @@ class Practice < ApplicationRecord
   belongs_to :user
 
   validates :date, uniqueness: { scope: :user_id }
-  validates :shooting_count, numericality: { greater_than_or_equal_to: 0 }
+  validates :shooting_count, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 500 }
 
   def start_time
     date
