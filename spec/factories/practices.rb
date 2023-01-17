62c20c4c30bb4928e2db72fbefbe85779bb442c6
taka110-IT <onyourmark_aska@yahoo.co.jp>
2023-01-17 16:01:21 +0900

    spec/models/practice_spec.rbにテストを作成

--- spec/factories/practices.rb
+++ spec/factories/practices.rb
@@ -2,6 +2,22 @@
 
 FactoryBot.define do
   factory :practice do
-    
+    sequence(:id) { |i| i }
+    date { '2023-1-1' }
+    shooting_count { 2 }
+    memo { 'memo' }
+    fixed { false }
+    important { false }
+    association :user
+
+    trait :important do
+      date { '2023-1-2' }
+      important { true }
+    end
+
+    trait :fixed do
+      date { '2023-1-3' }
+      fixed { true }
+    end
   end
 end
--- spec/models/practice_spec.rb
+++ spec/models/practice_spec.rb
@@ -1,5 +1,60 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Practice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:practice) { FactoryBot.create(:practice) }
+  let!(:practices) { described_class.setting_user_practices(described_class.all, practice.user) }
+  let!(:start_date) { described_class.display_start_date(practice).to_date }
+
+  before do
+    FactoryBot.create(:practice, :important, user: practice.user)
+    FactoryBot.create(:practice, :fixed, user: practice.user)
+    FactoryBot.create(:target, user: practice.user)
+  end
+
+  describe 'tab contents' do
+    context 'when important tab actived' do
+      it 'displayed important practices' do
+        memos = 'important'
+        important_practices = described_class.practices_tab_contents(memos)
+        expect(important_practices.length).to eq 1
+      end
+    end
+
+    context 'when all tab actived' do
+      it 'displayed all practices' do
+        memos = 'all'
+        all_practices = described_class.practices_tab_contents(memos)
+        expect(all_practices.length).to eq 3
+      end
+    end
+  end
+
+  describe 'set practices' do
+    it 'is practices set' do
+      expect(practices.length).to eq 3
+    end
+
+    it 'is fixed practice at top' do
+      expect(practices[0].date).to eq '2023-1-3'.to_date
+    end
+
+    it 'is descending order' do
+      expect(practices[1].date).to eq '2023-1-2'.to_date
+    end
+  end
+
+  it 'is results set' do
+    expect(described_class.setting_results(start_date, practice.user)).to eq 6
+  end
+
+  it 'is set remaining to target' do
+    results = described_class.setting_results(start_date, practice.user)
+    expect(described_class.remaining_to_target(Target.all, results)).to eq 4
+  end
+
+  it 'is created start_date from practice' do
+    expect(described_class.display_start_date(practice)).to eq '2023-1-1'
+  end
 end
