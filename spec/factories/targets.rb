92a5272ddaa88b7b9f3c40e1f031df52ac1eb392
taka110-IT <onyourmark_aska@yahoo.co.jp>
2023-01-17 12:22:26 +0900

    spec/models/target_spec.rbにテストを作成

--- spec/factories/targets.rb
+++ spec/factories/targets.rb
@@ -2,6 +2,10 @@
 
 FactoryBot.define do
   factory :target do
-    
+    total { 10 }
+    year { 2023 }
+    month { 1 }
+    achievement { false }
+    association :user
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :user do
     email { 'testuser@example.com' }
     password { 'Useruser1' }
+    confirmed_at { Date.today }
   end
 end
