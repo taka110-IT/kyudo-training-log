b393609e82c39bfb3aaa71f6c997658ff7c70752
taka110-IT <onyourmark_aska@yahoo.co.jp>
2023-02-20 10:10:13 +0900

    application_controller.rbにprivateを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   around_action :switch_locale
 
+  private
+
   def switch_locale(&)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &)
